d9242f0fe2040da9933359a4ead5ea968c1ce519
Kate <KhovanskayaK@yandex.ru>
2023-01-30 23:21:35 +0300

    добавлена задача 68

--- Program.cs
+++ Program.cs
@@ -12,13 +12,28 @@
 
 //Задача 66: Выполнить с помощью рекурсии.Задайте значения M и N. Напишите программу,
 //которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Console.Write("Введи число m: ");
+// int m  = int.Parse(Console.ReadLine()!);
+// Console.Write("Введи число n: ");
+// int n66  = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Сумма чисел от {m} до {n66}: {SumNumbers(m,n66)}");
+// // ------------------Метод------------------
+// int SumNumbers(int m, int n){
+//     if(m==n) return m;
+//     return n+SumNumbers(m, n-1);
+//}
+
+//Задача 68: Выполнить с помощью рекурсии.Напишите программу вычисления функции Аккермана
+//с помощью рекурсии. Даны два неотрицательных числа m и n.
 Console.Write("Введи число m: ");
-int m  = int.Parse(Console.ReadLine()!);
+int m68  = int.Parse(Console.ReadLine()!);
 Console.Write("Введи число n: ");
-int n66  = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"Сумма чисел от {m} до {n66}: {SumNumbers(m,n66)}");
-// ------------------Метод------------------
-int SumNumbers(int m, int n){
-    if(m==n) return m;
-    return n+SumNumbers(m, n-1);
+int n68  = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Функция Аккермана для чисел {m68} и {n68}: {AkkermanFunction(m68,n68)}");
+
+//----------Метод---------
+int AkkermanFunction(int m, int n){
+    if(m==0) return n+1;
+    if(n==0) return AkkermanFunction(m-1,1);
+    return AkkermanFunction(m-1, AkkermanFunction(m, n-1));
 }
